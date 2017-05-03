a91095a66b363e7d14a3a1d0b1209e3ce6791a66
Michael Adamski <mvadamski@gmail.com>
2017-05-03 14:12:40 -0400

    fixed all foundation and react issues save the main page header

--- react/src/components/User.js
+++ react/src/components/User.js
@@ -3,16 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const User = (props) => {
+
   return(
-    <div className='columns small-12 med-4 text-center'>
+    <div className='homepage-user-index column column-block'>
       <h1>
-        {props.username}
+        <a href={`/users/${props.id}`}>{props.username}</a>
       </h1>
-      {props.first_name}
-      {props.last_name}
-      {props.email}
-      {props.zip}
-      {props.mobile}
+      <img src={props.photo.url} alt="User Profile Picture" width="300px" height="auto"/>
+      <p>{props.first_name} {props.last_name}</p>
+      <p><strong>{props.email}</strong></p>
+      <p><i>PlaceHolder for Google Map</i></p>
+      <p><i>PlaceHolder for Online Learning</i></p>
       <hr width='50%'/>
     </div>
   )
