b7d5da75780f72638e91f1db1bd765ec80e9b5e8
Michael Adamski <mvadamski@gmail.com>
2017-05-06 14:58:14 -0400

    finally fixed the frickin db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Lesson.destroy_all
+
+
+teach=User.create(email: 'teach@bue.edu', password: 'hahacows', password_confirmation: 'hahacows', username: 'teach', first_name: 'teach', last_name: 'teach', is_student: false, zip: '02134')
+
+lesson=Lesson.create(instrument: 'guitar', skill_level: 'beginner', date: 'idk', teacher_id: teach.id)
+
+george=User.create(email: 'george53@bu.edu', password: 'hahacows', password_confirmation: 'hahacows', username: 'haha', first_name: 'haha', last_name: 'haha', is_student: true, zip: '01234')
+
+Tutorship.create(lesson: lesson, user: george)
+
+binding.pry
