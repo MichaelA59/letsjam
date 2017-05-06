5f24b94e3f540f895c6df8a7acc058f458d04a38
Michael Adamski <mvadamski@gmail.com>
2017-05-06 13:06:34 -0400

    more changes to db

--- db/migrate/20170505184217_create_lessons.rb
+++ db/migrate/20170505184217_create_lessons.rb
@@ -4,7 +4,8 @@ class CreateLessons < ActiveRecord::Migration[5.0]
       t.string :instrument, null: false
       t.string :skill_level, null: false
       t.string :date, null: false
-      t.integer :teacher_id
+      t.integer :teacher_id, null: false
+      t.integer :student_id
 
       t.timestamps
     end
